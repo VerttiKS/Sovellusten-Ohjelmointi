f7219c785f5210764a888df08b8e76b5ced837b9
VerttiKS <vertti.keski@gmail.com>
2023-09-20 07:25:34 +0300

    Tehtävä 3 valmis

---
+++ viikko1/ConstraitLayout/app/src/main/java/com/example/constraitlayout/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.constraitlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void changeColor(View view) {
+        int yellow = Color.parseColor("#f2d863");
+        view.setBackgroundColor(yellow);
+    }
+}
